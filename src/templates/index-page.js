e73eb75a9c5955cbe323bf91c5b3b584b3b07afe
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-01-27 12:50:27 -0600

    purple heading

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,9 +40,9 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+              'purple 0.5rem 0px 0px, purple -0.5rem 0px 0px',
+            backgroundColor: 'purple',
+            color: 'black',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -53,9 +53,9 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+              'purple 0.5rem 0px 0px, purple -0.5rem 0px 0px',
+            backgroundColor: 'purple',
+            color: 'black',
             lineHeight: '1',
             padding: '0.25em',
           }}
